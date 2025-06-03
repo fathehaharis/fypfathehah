4422cfa05f75ba093a900e35e9377d5d77d26a7f
fathehaharis <fathehaharis69@gmail.com>
2025-06-04 00:33:33 +0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+$host = '127.0.0.1';      // or 127.0.0.1
+$db   = 'timelesscarrental';     // your database name
+$user = 'root';           // your MySQL username
+$pass = '';               // your MySQL password ("" if none)
+$port = 3306;             // default MySQL port
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo " ";
+?>
+ 
